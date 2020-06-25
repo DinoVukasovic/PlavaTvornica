ded981c8949ee637b871b5a87b63009455ee4470
unknown <dino.vukasovic@yahoo.com>
2020-06-25 17:16:18 +0200

    a

--- JS/js2.js
+++ JS/js2.js
@@ -4,19 +4,18 @@ var rotation = false;
 var answer;
 for (i = 1; i <= numbers; i++) {
     answer = p;
+    p = p + 1;
 	if (i % 7 === 0) {
 		rotation = !rotation
+        p = p-1;
 	} else if (i % 13 === 0) {
 		if (rotation) {
 			p = p -1;
 		} else {
 			p = p+1;
 		}
-	} else if (!rotation){
-		p = p+1;
-	} else if (rotation) {
-		p = p-1;
-	}
+    }
+
 	if (p < 1) {
 		p = p+10;
 	} else if (p > 10) {
@@ -24,4 +23,6 @@ for (i = 1; i <= numbers; i++) {
 	}
 
 }
+console.log(p);
 console.log(answer);
+
